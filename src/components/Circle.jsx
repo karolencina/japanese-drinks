f48aab113bc54639108cc61db9aca7d2a73d6502
Karolina Lencina <karolencina@icloud.com>
2023-03-05 20:52:38 +0000

    Component updates

---
+++ src/components/Circle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NextBtn from "./buttons/NextBtn";
+
+const Circle = () => {
+    return (
+        <section>
+            <h1>Japanese Drinks</h1>
+            <p>Most people know what Japanese kitchen tastes like. But can the same be said about Japanese drinks? Click on the arrow and create your beverage!</p>
+            <NextBtn/>
+        </section>
+    )
+}
+
+export default Circle
